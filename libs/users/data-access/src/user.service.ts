ff8feb0609d16f392950053a1ef457df272c53d0
Reem Ben-David <reemile1996@gmail.com>
2025-08-10 11:30:40 +0300

    feat: Create data-access layer for users

---
+++ libs/users/data-access/src/user.service.ts
@@ -0,0 +1,31 @@
+import { inject, Injectable } from '@angular/core';
+
+import { User } from '@fusers/core/api-types';
+import { ApiService } from '@fusers/core/http-client';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class UserService {
+    readonly #apiService = inject(ApiService);
+
+    getUsers() {
+        return this.#apiService.get<User[]>('/users');
+    }
+
+    getUserById(id: string) {
+        return this.#apiService.get<User>(`/users/${id}`);
+    }
+
+    createUser(user: User) {
+        return this.#apiService.post<User, User>('/users', user);
+    }
+
+    updateUser(id: string, user: User) {
+        return this.#apiService.put<User, User>(`/users/${id}`, user);
+    }
+
+    deleteUser(id: string) {
+        return this.#apiService.delete<boolean>(`/users/${id}`);
+    }
+}
--- libs/users/users-feature/src/users.component.ts
+++ libs/users/users-feature/src/users.component.ts
@@ -5,5 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: './users.component.html',
 })
 export class UsersComponent {
-
+    
 }
