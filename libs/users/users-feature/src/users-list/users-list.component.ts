f70a479c0fa89299d8f1375871071b044cac0bfd
Reem Ben-David <reemile1996@gmail.com>
2025-08-10 15:47:09 +0300

    chore: Scaffold users-list component

---
+++ libs/users/users-feature/src/users-list/users-list.component.ts
@@ -0,0 +1,33 @@
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { User } from '@fusers/core/api-types';
+import { UserListItemComponent } from '../user-list-item/user-list-item.component';
+
+@Component({
+  selector: 'fusers-users-list',
+  standalone: true,
+  imports: [CommonModule, UserListItemComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './users-list.component.html',
+  styleUrls: ['./users-list.component.css']
+})
+export class UsersListComponent {
+  users = input<User[]>([]);
+  selectedUserId = input<string | null>(null);
+  
+  userSelect = output<string>();
+  editUser = output<User>();
+  deleteUser = output<User>();
+
+  onUserSelect(userId: string): void {
+    this.userSelect.emit(userId);
+  }
+
+  onEditUser(user: User): void {
+    this.editUser.emit(user);
+  }
+
+  onDeleteUser(user: User): void {
+    this.deleteUser.emit(user);
+  }
+}
