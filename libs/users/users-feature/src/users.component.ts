6aa70dae4c0966295446cd92d6353d00dc9aa073
Reem Ben-David <reemile1996@gmail.com>
2025-08-10 16:41:27 +0300

    chore: clean user component code

--- libs/users/users-feature/src/users.component.ts
+++ libs/users/users-feature/src/users.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
+
+import { UsersListComponent } from './users-list/users-list.component';
+import { UserOrdersComponent } from './user-orders/user-orders.component';
+
+import { User } from '@fusers/core/api-types';
+import { UsersStore } from '@fusers/users/data-access';
 
 @Component({
   selector: 'fusers-users',
+  standalone: true,
+  imports: [UsersListComponent, UserOrdersComponent],
   templateUrl: './users.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UsersComponent {
-    
+export class UsersComponent implements OnInit {
+  private readonly store = inject(UsersStore);
+
+  readonly users = this.store.userEntities;
+  readonly isLoading = this.store.loadUsersLoading;
+  readonly selectedUserId = this.store.selectedUserId;
+
+  ngOnInit(): void {
+    this.store.loadUsers();
+  }
+
+  onUserSelect(userId: string): void {
+    this.store.selectUser(userId);
+  }
+
+  onEditUser(user: User): void {
+    this.store.updateUser(user);
+  }
+
+  onDeleteUser(user: User): void {
+    this.store.removeUser(user.id);
+  }
 }
